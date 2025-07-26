fc16756497816665e43c457781b38aa82e767b9b
ZAID KHAN <kzaid6657@gmail.com>
2025-07-26 14:55:56 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,24 @@
+let score = "33abs"
+// console.log(typeof score)
+
+let valueInNumber = Number (score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33 
+// "33" => NaN not a number
+// true > 1 false > 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ // console.log(booleanIsLoggedIn);
+
+// when we pass 1 =>true num is 0 =>false 
+// ""=> false 
+// "zaid" =>true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(someNumber)
+console.log(typeof someNumber)
