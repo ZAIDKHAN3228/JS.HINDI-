a3e0b403e976c742adc184a29a9ece4a0cb461df
ZAID KHAN <kzaid6657@gmail.com>
2025-08-03 15:04:29 +0000

    datatypes-summery

---
+++ .devcontainer/01_basics/datatypes-summery.js
@@ -0,0 +1,36 @@
+// primitive data types 
+
+// 7 types : string, number, boolean, null, undefined, symbol, bigInt
+
+
+// const score = 100
+// const scoreValue = 100.3
+// console.log(scoreValue );
+
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol ('123')
+const anotherId = Symbol ('123')
+
+console.log(anotherId);
+
+const bigNumber = 99174176823234123323345343n
+
+// reference (non primitive datatypes)
+// array, object, functions
+
+const heros = ["shaktiman", "nagraj", "doga"];
+
+{
+    name: "zaid"
+    age: 22
+}
+
+ const myFunction = function(){};
+
+console.log("hello world");
+
+console.log(typeof id);
+
