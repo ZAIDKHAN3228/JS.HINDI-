5ff5f672c8f5889d224ac042aa01d6072bab0ba1
ZAID KHAN <kzaid6657@gmail.com>
2025-08-06 15:19:06 +0000

    05_strings

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,38 @@
+const name = "zaid"
+const repoCount = 50
+// console.log(name + repoCount + "value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('zaidhc')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('c'));
+
+const newString = gameName.substring (0, 4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+const newStingOne = "   zaid  "
+// console.log(newStingOne);
+// console.log(newStingOne.trim());
+
+const url = "https:/zaid.com/zaid%20khan"
+console.log(url.replace('%20','_' ));
+
+
+
+
+
+
+
+
