a85ce07e59d62d290a7b33d04a727cd49fd1d6b9
ZAID KHAN <kzaid6657@gmail.com>
2025-08-14 14:46:55 +0000

    datatypes summery

--- .devcontainer/01_basics/datatypes-summery.js
+++ .devcontainer/01_basics/datatypes-summery.js
@@ -1,5 +1,7 @@
 // primitive data types 
 
+const { useImperativeHandle } = require("react");
+
 // 7 types : string, number, boolean, null, undefined, symbol, bigInt
 
 
@@ -14,7 +16,7 @@ let userEmail;
 const id = Symbol ('123')
 const anotherId = Symbol ('123')
 
-console.log(anotherId);
+// console.log(anotherId);
 
 const bigNumber = 99174176823234123323345343n
 
@@ -30,7 +32,35 @@ const heros = ["shaktiman", "nagraj", "doga"];
 
  const myFunction = function(){};
 
-console.log("hello world");
+// console.log("hello world");
 
-console.log(typeof id);
+// console.log(typeof id);
 
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack (primitive) , heap (non-primitive)
+
+let myYoutubename = "yourszaid"
+let anothername = myYoutubename
+anothername = "zaididhere"
+
+
+// console.log(anothername);
+// console.log(myYoutubename);
+
+let userOne = {
+    email: "kzaid@gmail.com",
+    upi: "zaid@ybl"
+
+}
+
+let userTwo = userOne
+
+userTwo.email = "zaid@gmail.com"
+
+   console.log(userOne.email);
+   console.log(userTwo.email);
+   
