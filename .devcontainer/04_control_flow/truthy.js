4296884c596e33d36fcfa97ae29df33f3efba54b
ZAID KHAN <kzaid6657@gmail.com>
2025-09-21 14:30:10 +0000

    truthy and falsey

---
+++ .devcontainer/04_control_flow/truthy.js
@@ -0,0 +1,53 @@
+// const userEmail = []  
+// const userEmail = ""
+const userEmail = "z@zaid.ai"
+
+// if (userEmail) {
+//     console.log("Got user email");
+// } else {
+//     console.log("dont have user email");
+// }
+
+//  falsy values
+
+// false,0, -0, BigInt, on, "", null, undefined, NaN
+
+// truthy values
+
+// "0", "", [], {}, funcion(){}, new date etc
+
+// if (userEmail.length) {
+//     console.log("array is empty");
+// }
+
+// const  emptyobj = {}
+// if (Object.keys(emptyobj).length === 0) {
+//     console.log("object is empty");
+    
+// }
+
+// nullish coalesing operator
+let vall;
+// vall = 5 ?? 10
+// vall = null ?? 10
+// vall= undefined ?? 15
+// vall = null ?? 10 ?? 33
+
+
+
+// console.log(vall);
+
+//  terniary operator 
+
+// condition ? true : false 
+
+const iceteaprice = 100
+// iceteaprice <= 80 ? console.log("less than 80") : console.log("more than 80");
+
+const icecreamprice = 60
+// icecreamprice === 90 ? 
+// console.log("price is 60") : console.log("price is not 60");
+
+
+
+
