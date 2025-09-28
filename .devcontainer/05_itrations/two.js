2d86f2620d7a9cd4f220c5e438039670c830825b
ZAID KHAN <kzaid6657@gmail.com>
2025-09-28 11:45:03 +0000

    two loop

---
+++ .devcontainer/05_itrations/two.js
@@ -0,0 +1,25 @@
+
+let index =0
+while (index <=10) {
+    // console.log(`value is index ${index}`);
+    index = index + 1
+    
+}
+
+let myarray = ["zaid", "batsman", "superman"]
+
+let arr = 0
+
+while (arr <myarray.length) {
+   // console.log(`value is ${myarray[arr]}`);
+     
+    arr =arr + 1
+}
+
+let score = 11
+
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score <=10);
+
