bef017a7a1d24fb7edf36e57862dbbe24797eb00
ZAID KHAN <kzaid6657@gmail.com>
2025-09-29 13:59:16 +0000

    three loop

---
+++ .devcontainer/05_itrations/three.js
@@ -0,0 +1,44 @@
+// for of 
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    // console.log(num);
+    
+}
+
+const greetings = "hello zaid"
+for (const greet of greetings) {
+    // console.log(`each char is ${greet}`);
+    
+}
+
+// map 
+
+const map = new Map()
+map.set("IN", "india")
+map.set("USA", "united states of america")
+map.set("Fr", "france")
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+     console.log(key, ':-', value);
+     
+}
+
+const myOject ={
+    'game1': 'cricet',
+    'game2': 'football'
+}
+
+for (const [key, value] of object) {
+    console.log(key, ':-', value);
+    
+}
