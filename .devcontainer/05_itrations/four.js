7b0c2cd081bdfac2669543a721400725ff93e76e
ZAID KHAN <kzaid6657@gmail.com>
2025-10-01 04:09:03 +0000

    four forin loop

---
+++ .devcontainer/05_itrations/four.js
@@ -0,0 +1,22 @@
+// forin loop mostly used with objects
+
+const myObject = {
+    js: 'javascript',
+    py: 'python',
+    rb: 'ruby',
+    swift: 'swift for apple'
+}
+
+for (const key in myObject) {
+   
+  //  console.log(`${key} is short for ${myObject[key]}`)
+    
+}
+    
+
+const myArray = ["javascript", "python",  "ruby", "cpp"]
+for (const key in myArray) {
+    // console.log(`${key} is at index ${myArray}[key]`);
+
+}
+
