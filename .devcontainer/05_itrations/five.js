9f5cdf2c1c52286bb56c27a6eda99288399be8a7
ZAID KHAN <kzaid6657@gmail.com>
2025-10-01 04:54:08 +0000

    five object in array

---
+++ .devcontainer/05_itrations/five.js
@@ -0,0 +1,44 @@
+const coding = ["javascript","python","java","cpp","ruby"];
+
+coding.forEach(function (val){
+   // console.log(val);
+    
+})
+
+
+coding.forEach((item) => {
+  //   console.log(item);
+    
+})
+
+coding.forEach(val => {
+   // console.log(val);
+    
+})
+
+// coding.forEach((val, index, arr) => {
+    //  console.log(val, index, arr);
+     
+//})
+
+
+const myCoding = [
+ {
+    languagename : "javascript",
+     languagefilename : "js"
+ },
+ {
+    languagename: "python",
+     languagefilename: "py"
+ },
+ {
+    languagename: "java",
+    languagefilename: "java"
+ },
+
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languagename);
+    
+})
